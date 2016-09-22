4f267ba1f76686a7a921732bafed0c81e53cbe0d
Alexis Couronne <alexis.couronne@gandi.net>
2016-09-22 09:08:41 +0200

    Define rotors I, II, III, IV and V

--- src/Enigma.elm
+++ src/Enigma.elm
@@ -1,4 +1,9 @@
 module Enigma exposing 
+  ( rotorI
+  , rotorII
+  , rotorIII
+  , rotorIV
+  , rotorV
   , reflectorB
   , reflectorC
   )
@@ -6,13 +11,47 @@ module Enigma exposing
 {-| This library is an implementation of an Enigma machine.
 
 # Definition
+@docs rotorI
+@docs rotorII
+@docs rotorIII
+@docs rotorIV
+@docs rotorV
 @docs reflectorB
 @docs reflectorC
 
 -}
 
-
+import Enigma.Rotor exposing
+  ( Rotor
+  , buildRotor
+  , initRotor
+  )
 import Enigma.Reflector exposing (..)
+
+
+{-| Define rotorI -}
+rotorI : Rotor
+rotorI = buildRotor "EKMFLGDQVZNTOWYHXUSPAIBRCJ" 'Q'
+
+
+{-| Define rotorII -}
+rotorII : Rotor
+rotorII = buildRotor "AJDKSIRUXBLHWTMCQGZNPYFVOE" 'E'
+
+
+{-| Define rotorIII -}
+rotorIII : Rotor
+rotorIII = buildRotor "BDFHJLCPRTXVZNYEIWGAKMUSQO" 'V'
+
+
+{-| Define rotorIV -}
+rotorIV : Rotor
+rotorIV = buildRotor "ESOVPZJAYQUIRHXLNFTGKDCMWB" 'J'
+
+
+{-| Define rotorV -}
+rotorV : Rotor
+rotorV = buildRotor "VZBRGITYUPSDNHLXAWMJQOFECK" 'Z'
 
 
 {-| Define reflectorB -}
