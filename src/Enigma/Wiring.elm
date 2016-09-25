a5f58c1d8c1f94d9dd562775ac7ef114548a6442
Alexis Couronne <alexis.couronne@gandi.net>
2016-09-25 21:13:47 +0200

    Implement Wiring.initWiring function

--- src/Enigma/Wiring.elm
+++ src/Enigma/Wiring.elm
@@ -15,3 +15,19 @@ initWire input output =
   { input = input
   , output = output
   }
+
+
+type alias Wiring =
+  List Wire
+
+
+initWiring: String -> String -> Maybe Wiring
+initWiring inputAlphabet outputAlphabet =
+  if String.length inputAlphabet == String.length outputAlphabet then
+    let
+      inputs = String.toList inputAlphabet
+      outputs = String.toList outputAlphabet
+    in
+     Just (List.map2 initWire inputs outputs)
+  else
+    Nothing
--- tests/WiringTests.elm
+++ tests/WiringTests.elm
@@ -14,4 +14,22 @@ all =
             Wiring.initWire 'A' 'C'
               |> Expect.equal { input = 'A', output = 'C' }
       ]
+    , describe "initWiring"
+      [ test "shoud return Nothing" <|
+          \() ->
+            Wiring.initWiring "ABCDE" "ZYX"
+              |> Expect.equal Nothing
+      , test "should return a Wiring record" <|
+          \() ->
+            let expectedValue =
+              [ { input = 'A', output = 'Z' }
+              , { input = 'B', output = 'Y' }
+              , { input = 'C', output = 'X' }
+              , { input = 'D', output = 'W' }
+              , { input = 'E', output = 'V' }
+              ]
+            in
+              Wiring.initWiring "ABCDE" "ZYXWV"
+                |> Expect.equal (Just expectedValue)
+      ]
     ]
